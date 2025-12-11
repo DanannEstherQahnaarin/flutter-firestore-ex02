5adef1003b33d538881f48bac185638a8b4bf0d5
kmg <deepestdark@gmail.com>
2025-12-11 13:58:59 +0900

    init

---
+++ android/app/src/main/kotlin/com/example/flutter_application_mms/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_application_mms
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_mms/firebase_options.dart';
+
+void main() async {
+  // Flutter 엔진과 위젯 바인딩이 초기화될 때까지 기다립니다.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebase 초기화
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: Center(child: Text('Config Complate')),
+    );
+  }
+}
