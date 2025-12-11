43aaa649c6b314028446ad2938453469d066c18b
Qahnaarin <deepestdark@gmail.com>
2025-12-11 20:34:24 +0900

    파일 변경 및 추가

--- lib/models/member.dart
+++ lib/models/member.dart
@@ -1,16 +1,19 @@
+enum UserRole { user, admin }
+
 class Member {
   final String id;
   final String uid;
   final String name;
   final String email;
-
+  final UserRole userRole;
   final DateTime timestamp;
 
   Member({
     required this.id,
-    this.uid = '',
+    required this.uid,
     required this.name,
     required this.email,
+    required this.userRole,
     required this.timestamp,
   });
 
@@ -21,9 +24,14 @@ class Member {
 
     return Member(
       id: id,
-      name: (map['name'] ?? '') as String,
+      uid: (map['name'] ?? '') as String,
+      name: (map['uid'] ?? '') as String,
       email: (map['email'] ?? '') as String,
       timestamp: timestamp,
+      userRole: UserRole.values.firstWhere(
+        (e) => e.name == map['userRole'],
+        orElse: () => UserRole.user,
+      ),
     );
   }
 
